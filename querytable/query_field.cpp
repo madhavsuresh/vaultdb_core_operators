b9ef0eebc0e6db5815cf07dec84ef8abfcc3329e
Madhav Suresh <madhav@thoughtbison.net>
2020-01-27 18:08:57 -0600

    GetValue needs to be const qualified

--- querytable/query_field.cpp
+++ querytable/query_field.cpp
@@ -67,6 +67,7 @@ QueryField::QueryField(std::string val, int fn)
   this->unencrypted_value.stringValue = tmp;
   assert(strlen(this->unencrypted_value.stringValue) == (val.length()));
 }
-types::Value *QueryField::GetValue() {
+
+types::Value *QueryField::GetValue() const {
   return value_.get();
 }
