e7dd9b93666afadb6dc83059fd41e1fdcbd8c24f
Madhav Suresh <madhav@thoughtbison.net>
2020-02-17 18:37:32 -0600

    secure join now works
    	* Outputs the full merged tuples

--- main.cpp
+++ main.cpp
@@ -17,27 +17,25 @@ DEFINE_int32(int_input, 10, "int_intput");
 
 int main(int argc, char **argv) {
   gflags::ParseCommandLineFlags(&argc, &argv, true /* remove_flags */);
-  /*
   emp::NetIO *io = new emp::NetIO(
       FLAGS_party == emp::ALICE ? nullptr : FLAGS_hostname.c_str(), FLAGS_port);
   setup_semi_honest(io, FLAGS_party);
-   */
 
   PQDataProvider pq = PQDataProvider();
   auto lineitem =
       pq.GetQueryTable("dbname=tpch_unioned",
-                       "SELECT l_orderkey, l_suppkey FROM lineitem LIMIT 5000");
+                       "SELECT l_orderkey, l_suppkey FROM lineitem LIMIT 50");
   auto orders = pq.GetQueryTable("dbname=tpch_unioned",
-                                 "SELECT o_orderkey FROM orders LIMIT 5000");
+                                 "SELECT o_orderkey FROM orders LIMIT 50");
 
   auto supplier = pq.GetQueryTable("dbname=tpch_unioned",
-                                   "SELECT s_suppkey FROM supplier LIMIT 100");
-  /*
+                                   "SELECT s_suppkey FROM supplier LIMIT 50");
+
   EmpParty my_party =
       FLAGS_party == emp::ALICE ? EmpParty::ALICE : EmpParty::BOB;
   ShareDef def;
-  ShareCount ca = {.party = EmpParty::ALICE, .num_tuples = 100};
-  ShareCount cb = {.party = EmpParty::BOB, .num_tuples = 100};
+  ShareCount ca = {.party = EmpParty::ALICE, .num_tuples = 50};
+  ShareCount cb = {.party = EmpParty::BOB, .num_tuples = 50};
 
   def.share_map[EmpParty::ALICE] = ca;
   def.share_map[EmpParty::BOB] = cb;
@@ -46,18 +44,19 @@ int main(int argc, char **argv) {
   auto s_orders = ShareData(orders->GetSchema(), my_party, orders.get(), def);
   auto s_supplier =
       ShareData(supplier->GetSchema(), my_party, supplier.get(), def);
-  */
 
   JoinDef x = {.left_index = 0, .right_index = 0};
   x.id = vaultdb::expression::ExpressionId::EQUAL;
   auto start = std::chrono::system_clock::now();
-  auto s_OL = Join(orders.get(), lineitem.get(), x);
+  auto s_OL = Join(s_orders.get(), s_lineitem.get(), x);
   auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::chrono::duration<double> elapsed_seconds = end - start;
   std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
   std::cout << "finished computation at " << std::ctime(&end_time)
             << "elapsed time: " << elapsed_seconds.count() << "s\n";
-  JoinDef y = {.left_index = 0, .right_index = 1};
-  Join(supplier.get(), lineitem.get(), x);
+  JoinDef y = {.left_index = 0, .right_index = 0};
+  auto s_OLS = Join(s_OL.get(), s_supplier.get(), x);
+  std::cout << "NUM TUPLES :" << s_OL->GetNumTuples() << ", "
+            << s_OLS->GetNumTuples();
 }
--- operators/secure_join.cpp
+++ operators/secure_join.cpp
@@ -7,56 +7,59 @@
 #include <querytable/expression/expression.h>
 #include <querytable/query_table.h>
 
-void MergeTuples(QueryTuple *output, QueryTuple **tuples,
-                 const ProjectList &def) {
-  for (auto &p : def.pl) {
-    auto f = tuples[p.in_table_index]->GetField(p.in_field_index);
-    output->PutField(p.out_field_index, f);
+void MergeTuples(QueryTuple *output, const QueryTuple *tuple1,
+                 const QuerySchema *schema1, const QueryTuple *tuple2,
+                 const QuerySchema *schema2) {
+  int counter = 0;
+  for (int i = 0; i < schema1->GetNumFields(); i++) {
+    output->PutField(counter, tuple1->GetField(i));
+    counter++;
+  }
+
+  for (int i = 0; i < schema2->GetNumFields(); i++) {
+    output->PutField(counter, tuple2->GetField(i));
+    counter++;
   }
 }
 
-std::unique_ptr<QuerySchema> MergeSchema(std::vector<QuerySchema *> &schemas,
-                                         const ProjectList &def) {
-  std::unique_ptr<QuerySchema> output =
-      std::make_unique<QuerySchema>(def.pl.size());
-  for (auto &p : def.pl) {
-    output->PutField(p.out_field_index,
-                     *schemas.at(p.in_table_index)->GetField(p.in_field_index));
+std::unique_ptr<QuerySchema> MergeSchema(const QuerySchema *schema1,
+                                         const QuerySchema *schema2) {
+  std::unique_ptr<QuerySchema> output = std::make_unique<QuerySchema>(
+      schema1->GetNumFields() + schema2->GetNumFields());
+  int counter = 0;
+  for (int i = 0; i < schema1->GetNumFields(); i++) {
+    output->PutField(counter, *schema1->GetField(i));
+    counter++;
+  }
+  for (int i = 0; i < schema2->GetNumFields(); i++) {
+    output->PutField(counter, *schema2->GetField(i));
+    counter++;
   }
   return output;
 }
 
 std::unique_ptr<QueryTable> Join(QueryTable *left, QueryTable *right,
                                  const JoinDef &def) {
-  // auto qt =
-  // std::make_unique<QueryTable>(left->GetNumTuples()*right->GetNumTuples());
-  QueryTuple *pre_allocated = static_cast<QueryTuple *>(malloc(
-      sizeof(QueryTuple) * left->GetNumTuples() * right->GetNumTuples()));
-  ProjectList pdef;
-  pdef.pl.emplace_back(0, 0, 0);
-  pdef.pl.emplace_back(1, 0, 1);
-  pdef.pl.emplace_back(1, 1, 2);
-
+  std::unique_ptr<QueryTable> output_table = std::make_unique<QueryTable>(
+      left->GetIsEncrypted(), left->GetNumTuples() * right->GetNumTuples());
+  output_table->SetSchema(MergeSchema(left->GetSchema(), right->GetSchema()));
   int index = 0;
-  for (auto &lt : *left) {
-    for (auto &rt : *right) {
-      expression::Expression ex(lt.GetField(def.left_index)->GetValue(),
-                                rt.GetField(def.right_index)->GetValue(),
+  for (int li = 0; li < left->GetNumTuples(); li++) {
+    for (int ri = 0; ri < right->GetNumTuples(); ri++) {
+      auto lt = left->GetTuple(li);
+      auto rt = right->GetTuple(ri);
+      expression::Expression ex(lt->GetField(def.left_index)->GetValue(),
+                                rt->GetField(def.right_index)->GetValue(),
                                 def.id);
-
-      QueryTuple *t[10];
-      t[0] = &lt;
-      t[1] = &rt;
       auto output = ex.execute();
-
-      pre_allocated[index].SetIsEncrypted(left->GetIsEncrypted());
-      pre_allocated[index].InitDummy();
-      MergeTuples(&pre_allocated[index], t, pdef);
-      pre_allocated[index].SetDummyFlag(&output);
-      // qt->PutTuple(std::move(tmp));
+      output_table->GetTuple(index)->SetIsEncrypted(
+          output_table->GetIsEncrypted());
+      output_table->GetTuple(index)->InitDummy();
+      output_table->GetTuple(index)->SetDummyFlag(&output);
+      MergeTuples(output_table->GetTuple(index), lt, left->GetSchema(), rt,
+                  right->GetSchema());
       index++;
     }
   }
-  std::cout << sizeof(QueryTuple) << "," << sizeof(types::Value);
-  return nullptr;
+  return output_table;
 }
